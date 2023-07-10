488d8ec93b40445658d8ad676f5218c40c0ae8bc
MUKARRAM-PC\Mukarram <mukarramjavid@gmail.com>
2023-07-10 22:39:28 +0500

    - Add blank pages for graphic designing service

--- CTD/CTD/Controllers/HomeController.cs
+++ CTD/CTD/Controllers/HomeController.cs
@@ -118,7 +118,21 @@ namespace CTD.Controllers
         {
             return View();
         }
-
+        [Route("{cityName}-graphic-designing")]
+        public IActionResult CitywiseGraphicDesigning(string cityName)
+        {
+            if (!string.IsNullOrEmpty(cityName))
+            {
+                string viewName = cityName.GraphicDesigningServicesViewNameByCity();
+                if (!string.IsNullOrEmpty(viewName))
+                    return View($"{_servicesViewPath}/GraphicDesigning/{viewName}.cshtml");
+                return View(_commingSoonViewPath);
+            }
+            else
+            {
+                return View(_commingSoonViewPath);
+            }
+        }
         #endregion
 
         #region SEO 
