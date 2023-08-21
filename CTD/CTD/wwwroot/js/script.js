73bf4e7f293edb7951f8cf75456146e35a3bf473
MUKARRAM-PC\Mukarram <mukarramjavid@gmail.com>
2023-08-21 23:46:46 +0500

    update script

--- CTD/CTD/wwwroot/js/script.js
+++ CTD/CTD/wwwroot/js/script.js
@@ -687,28 +687,28 @@
 	
 	
 	//Contact Form Validation
-	//if($('#contact-form').length){
-	//	$('#contact-form').validate({
-	//		rules: {
-	//			username: {
-	//				required: true
-	//			},
-	//			email: {
-	//				required: true,
-	//				email: true
-	//			},
-	//			phone: {
-	//				required: true
-	//			},
-	//			subject: {
-	//				required: true
-	//			},
-	//			message: {
-	//				required: true
-	//			}
-	//		}
-	//	});
-	//}
+	if($('#contact-form').length){
+		$('#contact-form').validate({
+			rules: {
+				username: {
+					required: true
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				phone: {
+					required: true
+				},
+				subject: {
+					required: true
+				},
+				message: {
+					required: true
+				}
+			}
+		});
+	}
 	
 	
 	
